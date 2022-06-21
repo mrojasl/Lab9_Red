2621afe051282dd03b01b22f2b8e632f0d9e7023
Loammi Jezreel Ugaz Manayay <a20191417@pucp.edu.pe>
2022-06-20 20:23:40 -0500

    Creación de los beans

---
+++ src/main/java/pe/edu/pucp/lab9_red/beans/Humano.java
@@ -0,0 +1,49 @@
+package pe.edu.pucp.lab9_red.beans;
+
+public class Humano {
+    private String idHumano;
+    private String nombre;
+    private String apellido;
+    private String sexo;
+    private boolean estadoZombie;
+
+    public String getIdHumano() {
+        return idHumano;
+    }
+
+    public void setIdHumano(String idHumano) {
+        this.idHumano = idHumano;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public boolean isEstadoZombie() {
+        return estadoZombie;
+    }
+
+    public void setEstadoZombie(boolean estadoZombie) {
+        this.estadoZombie = estadoZombie;
+    }
+}
