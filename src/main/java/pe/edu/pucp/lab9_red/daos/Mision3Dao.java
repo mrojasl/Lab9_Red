baac4c0a793ce6bdc12250c108bd957cde689fa7
Marcelo Rojas PC <mrojasl@pucp.edu.pe>
2022-06-21 11:06:22 -0500

    Avance mision3

--- src/main/java/pe/edu/pucp/lab9_red/daos/Mision3Dao.java
+++ src/main/java/pe/edu/pucp/lab9_red/daos/Mision3Dao.java
@@ -1,2 +1,25 @@
-package pe.edu.pucp.lab9_red.daos;public class Mision3Dao {
+package pe.edu.pucp.lab9_red.daos;
+
+import pe.edu.pucp.lab9_red.beans.Virus;
+
+import java.util.ArrayList;
+
+public class Mision3Dao {
+
+    public static ArrayList<Virus> listarVirus(){
+        ArrayList<Virus> listaVirus = new ArrayList<>();
+        String sql="select ";
+
+
+
+
+
+
+
+
+
+
+        return listaVirus;
+    }
+
 }
